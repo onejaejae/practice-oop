f800a96f7cc369b3b49e56d99865f240e046a680
wonjjee <yoteamo7@naver.com>
2024-02-05 10:58:49 +0900

    refactor: added setNestApp file

---
+++ setNestApp.ts
@@ -0,0 +1,26 @@
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { CustomValidationException } from 'src/core/exception/custom.validation.exception';
+
+export function setNestApp<T extends INestApplication>(app: T): void {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      validationError: {
+        value: true,
+      },
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new BadRequestException(
+          validationErrors.map((e) => new CustomValidationException(e)),
+        );
+      },
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+}
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,11 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { Modules } from './modules';
-import {
-  ClassSerializerInterceptor,
-  ValidationPipe,
-  ValidationError,
-  BadRequestException,
-} from '@nestjs/common';
-import { CustomValidationException } from './core/exception/custom.validation.exception';
+import { setNestApp } from 'setNestApp';
 
 async function bootstrap() {
   const app = await NestFactory.create(Modules);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      validationError: {
-        value: true,
-      },
-      exceptionFactory: (validationErrors: ValidationError[] = []) => {
-        return new BadRequestException(
-          validationErrors.map((e) => new CustomValidationException(e)),
-        );
-      },
-    }),
-  );
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  setNestApp(app);
 
   await app.listen(3000);
 }
