cb2099f6c239e8e52bd266c9b16ca5a7bd23b6c6
wonjjee <yoteamo7@naver.com>
2024-02-06 12:10:44 +0900

    fix: Encrypt class

--- src/common/util/encrypt.ts
+++ src/common/util/encrypt.ts
@@ -1,14 +1,14 @@
-import * as argon2 from 'argon2';
+import { hash, verify } from 'argon2';
 
 export class Encrypt {
   static async createHash(value: string): Promise<string> {
-    return argon2.hash(value);
+    return hash(value);
   }
 
   static async isSameAsHash(
     plainPassword: string,
     hashedPassword: string,
   ): Promise<boolean> {
-    return argon2.verify(plainPassword, hashedPassword);
+    return verify(hashedPassword, plainPassword);
   }
 }
